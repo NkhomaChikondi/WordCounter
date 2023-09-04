bb3c873c1cecd8910ab678e44ba624fcfc6036ce
Chikondi Nkhoma <61491585+NkhomaChikondi@users.noreply.github.com>
2023-09-04 11:17:26 +0200

    implemented the IdataManager in the dataManager service

---
+++ Interfaces/IDataManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCounter.Interfaces
+{
+    public interface IDataManager
+    {
+        void getFile(string[] args);
+        int wordCount(string filePath);
+    }
+}
---
+++ Services/DataManagerService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordCounter.Interfaces;
+
+namespace WordCounter.Services
+{
+    public class DataManagerService : IDataManager
+    {
+        public void getFile(string[] args)
+        {
+            int linesInTotal = 0, wordsIntotal = 0;
+            long bytesInTotal = 0;
+            // loop through all the files that are passed from from the command line argument
+            foreach (string file in args)
+            {
+              // check if the file exist
+              if(File.Exists(file))
+              {
+                // initialize a wrapper for file path
+                var fileInfor = new FileInfo(file);
+                    int lines = File.ReadLines(file).Count();
+                    // call the wordCount method 
+                    int words = wordCount(file);
+                    long bytes = fileInfor.Length;
+
+                    linesInTotal += lines;
+                    wordsIntotal += words;
+                    bytesInTotal += bytes;
+                    Console.WriteLine($"{lines}\t{words}\t{bytes}\t{file}");
+              }
+                else
+                {
+                    Console.WriteLine($"File not found: {file}");
+                }
+            }
+            Console.WriteLine($"{linesInTotal}\t{wordsIntotal}\t{bytesInTotal}\tTotal");
+        }
+
+        public int wordCount(string filePath)
+        {
+            string text = File.ReadAllText(filePath);
+            string[] words = text.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            return words.Length;
+        }
+    }
+}
